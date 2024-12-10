3e3491c16c35819b0c4340a9440ef0a0590a88f8
BugHunter-X <askzenai@gmail.com>
2024-12-10 06:10:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,13 @@
+function greet(name: string | null): string {
+  if (name === null) {
+    return "Hello, world!";
+  } else {
+    return `Hello, ${name}!";
+  }
+}
+
+//Alternative using optional chaining:
+
+function greet2(name: string | null): string {
+  return `Hello, ${name ?? 'world'}!`;
+}
